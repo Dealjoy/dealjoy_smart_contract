329315f45ae02e28acca610383853e35fb715486
Lauri Peltonen <pelto@iki.fi>
2018-07-24 12:10:05 +0300

    Initial structure: truffle, required OpenZeppelin contracts

---
+++ contracts/crowdsale/Crowdsale.sol
@@ -0,0 +1,122 @@
+pragma solidity ^0.4.24;
+
+import "../math/SafeMath.sol";
+
+
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a base contract for managing a token crowdsale,
+ * allowing investors to purchase tokens with ether. This contract implements
+ * such functionality in its most fundamental form and can be extended to provide additional
+ * functionality and/or custom behavior.
+ * The external interface represents the basic interface for purchasing tokens, and conform
+ * the base architecture for crowdsales. They are *not* intended to be modified / overridden.
+ * The internal interface conforms the extensible and modifiable surface of crowdsales. Override
+ * the methods to add functionality. Consider using 'super' where appropriate to concatenate
+ * behavior.
+ */
+contract Crowdsale {
+  using SafeMath for uint256;
+
+  // Address where funds are collected
+  address public wallet;
+
+  // Amount of wei raised
+  uint256 public weiRaised;
+
+  /**
+   * @param _wallet Address where collected funds will be forwarded to
+   */
+  constructor(address _wallet) public {
+    require(_wallet != address(0));
+    wallet = _wallet;
+  }
+
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
+
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param _beneficiary Address performing the token purchase
+   */
+  function buyTokens(address _beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    _updatePurchasingState(_beneficiary, weiAmount);
+
+    _forwardFunds();
+    _postValidatePurchase(_beneficiary, weiAmount);
+  }
+
+  // -----------------------------------------
+  // Internal interface (extensible)
+  // -----------------------------------------
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use `super` in contracts that inherit from Crowdsale to extend their validations.
+   * Example from CappedCrowdsale.sol's _preValidatePurchase method: 
+   *   super._preValidatePurchase(_beneficiary, _weiAmount);
+   *   require(weiRaised.add(_weiAmount) <= cap);
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _preValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+  {
+    require(_beneficiary != address(0));
+    require(_weiAmount != 0);
+  }
+
+  /**
+   * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid conditions are not met.
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _postValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+  {
+    // optional override
+  }
+
+
+  /**
+   * @dev Override for extensions that require an internal state to check for validity (current user contributions, etc.)
+   * @param _beneficiary Address receiving the tokens
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _updatePurchasingState(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+  {
+    // optional override
+  }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+}
---
+++ contracts/crowdsale/OwnCrowdsale.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.24;
+
+import "../math/SafeMath.sol";
+import "./validation/WhitelistedCrowdsale.sol";
+import "./validation/TimedCrowdsale.sol";
+import "./validation/CappedCrowdsale.sol";
+import "../lifecycle/Pausable.sol";
+import "./Crowdsale.sol";
+
+contract OwnCrowdsale is WhitelistedCrowdsale, TimedCrowdsale, CappedCrowdsale, Pausable {
+
+	uint256 minInvestment;
+
+  constructor(
+	address _wallet, 
+	uint256 _minInvestment,
+	uint256 _openingTime,
+	uint256 _closingTime,
+	uint256 _cap
+	)
+  Crowdsale(_wallet)
+  TimedCrowdsale(_openingTime, _closingTime)
+  CappedCrowdsale(_cap)
+  public {
+	minInvestment = _minInvestment;
+  }
+  
+  /**
+   * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use `super` in contracts that inherit from Crowdsale to extend their validations.
+   * Example from CappedCrowdsale.sol's _preValidatePurchase method: 
+   *   super._preValidatePurchase(_beneficiary, _weiAmount);
+   *   require(weiRaised.add(_weiAmount) <= cap);
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _preValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    whenNotPaused
+    internal
+  {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+	require(_weiAmount >= minInvestment, "Sorry, minimum investment not met");
+  }
+}
